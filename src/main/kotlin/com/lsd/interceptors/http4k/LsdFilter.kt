0e9f08c2483f3c4534467b33d2c25a323388a2eb
nickmcdowall <nicholas.mcdowall@gmail.com>
2023-05-16 15:54:38 +0100

    fix: exclude known binary request/response bodies

--- src/main/kotlin/com/lsd/interceptors/http4k/LsdFilter.kt
+++ src/main/kotlin/com/lsd/interceptors/http4k/LsdFilter.kt
@@ -15,26 +15,22 @@ import java.util.zip.ZipInputStream
 
 class LsdFilterProvider(
     lsd: LsdContext,
+    val showCompressedBody: Boolean = false,
     sourceNameProvider: (Request) -> String = ::extractSourceName,
     targetNameProvider: (Request) -> String = ::extractTargetName,
     nameCleaner: (String) -> String = String::normalise
 ) {
 
-    val filter =
-        Filter { next ->
-            { request ->
-                val sourceName = nameCleaner(sourceNameProvider(request))
-                val targetHost = nameCleaner(targetNameProvider(request))
-                val startTime = Instant.now()
-                val requestBodyCopy = request.body.payload.array()
-                lsd.capture(
-                    messageBuilder()
-                        .id(lsd.idGenerator.next())
-                        .from(sourceName)
-                        .to(targetHost)
-                        .label("${request.method} ${request.uri}")
-                        .data(
-                            """
+    val filter = Filter { next ->
+        { request ->
+            val sourceName = nameCleaner(sourceNameProvider(request))
+            val targetHost = nameCleaner(targetNameProvider(request))
+            val startTime = Instant.now()
+            val requestBodyCopy = request.body.payload.array()
+            lsd.capture(
+                messageBuilder().id(lsd.idGenerator.next()).from(sourceName).to(targetHost)
+                    .label("${request.method} ${request.uri}").data(
+                        """
                            | <section>
                            |   <h3>PATH</h3>
                            |   <p>${sourceName}: ${request.method} to (${targetHost})${request.uri}</p>
@@ -42,22 +38,26 @@ class LsdFilterProvider(
                            | ${renderHeaders(request.headers)}
                            | <section>
                            |    <h3>BODY</h3>
-                           |    <p>${readBody(requestBodyCopy.inputStream(), request.header("accept-encoding"))}</p>  
+                           |    <p>${
+                            readBody(
+                                inputStream = requestBodyCopy.inputStream(),
+                                contentType = request.header("Content-Type"),
+                                encoding = request.header("Content-Encoding")
+                            )
+                        }</p>  
                            | </section>
                            |""".trimMargin()
 
-                        ).build()
-                )
-                val originalResponse = next(request.body(requestBodyCopy.inputStream()))
-                val responseBodyCopy = originalResponse.body.payload.array()
-                val durationMillis = Instant.now().toEpochMilli() - startTime.toEpochMilli()
+                    ).build()
+            )
+            val originalResponse = next(request.body(requestBodyCopy.inputStream()))
+            val responseBodyCopy = originalResponse.body.payload.array()
+            val durationMillis = Instant.now().toEpochMilli() - startTime.toEpochMilli()
 
-                originalResponse.body(responseBodyCopy.inputStream()).also { response ->
-                    lsd.capture(
-                        messageBuilder()
-                            .id(lsd.idGenerator.next())
-                            .data(
-                                """
+            originalResponse.body(responseBodyCopy.inputStream()).also { response ->
+                lsd.capture(
+                    messageBuilder().id(lsd.idGenerator.next()).data(
+                        """
                                    | <section>
                                    |   <h3>RESPONSE</h3>
                                    |   <p>$targetHost to $sourceName (${durationMillis}ms)</p>
@@ -66,52 +66,61 @@ class LsdFilterProvider(
                                    | <section>
                                    |   <h3>BODY</h3>
                                    |   ${
-                                    readBody(
-                                        responseBodyCopy.inputStream(),
-                                        response.header("content-encoding")
-                                    )
-                                }        
+                            readBody(
+                                inputStream = responseBodyCopy.inputStream(),
+                                contentType = response.header("Content-Type"),
+                                encoding = response.header("Content-Encoding")
+                            )
+                        }        
                                    | </section>
                                 """.trimMargin()
-                            )
-                            .from(targetHost)
-                            .to(sourceName)
-                            .type(SYNCHRONOUS_RESPONSE)
-                            .label("${response.status} (${durationMillis}ms) ")
-                            .colour(
-                                when (response.status.code) {
-                                    in 200..299 -> "green"
-                                    in 300..399 -> "purple"
-                                    in 400..499 -> "orange"
-                                    in 500..599 -> "red"
-                                    else -> "black"
-                                }
-                            ).build()
-                    )
-                }
+                    ).from(targetHost).to(sourceName).type(SYNCHRONOUS_RESPONSE)
+                        .label("${response.status} (${durationMillis}ms) ").colour(
+                            when (response.status.code) {
+                                in 200..299 -> "green"
+                                in 300..399 -> "purple"
+                                in 400..499 -> "orange"
+                                in 500..599 -> "red"
+                                else -> "black"
+                            }
+                        ).build()
+                )
             }
         }
+    }
+
+    private fun readBody(inputStream: ByteArrayInputStream, contentType: String?, encoding: String?): String {
+        if (encoding != null && !showCompressedBody) 
+            return "[encoded body: ${encoding}]"
+        
+        return when {
+            contentType == null || containsSupported(contentType) ->
+                when (encoding) {
+                    "gzip" -> GZIPInputStream(inputStream).bufferedReader(Charsets.UTF_8).use { it.readText() }
+                        .escapeHtml()
+
+                    "zip" -> ZipInputStream(inputStream).bufferedReader(Charsets.UTF_8).use { it.readText() }
+                        .escapeHtml()
+
+                    else -> inputStream.bufferedReader(Charsets.UTF_8).use(BufferedReader::readText).escapeHtml()
+                }
+
+            else -> "[${contentType}]"
+        }
+    }
 }
 
 fun extractSourceName(it: Request) = it.header("User-Agent") ?: "Anonymous"
 
-fun extractTargetName(it: Request): String = (
-        it.header("Host")
-            ?: it.uri.host.split(".").first()
-        ).ifBlank { "Unknown" }
+fun extractTargetName(it: Request): String = (it.header("Host") ?: it.uri.host.split(".").first()).ifBlank { "Unknown" }
 
-private fun readBody(inputStream: ByteArrayInputStream, encoding: String?): String =
-    when (encoding) {
-        "gzip" -> GZIPInputStream(inputStream).bufferedReader(Charsets.UTF_8).use { it.readText() }.escapeHtml()
-        "zip" -> ZipInputStream(inputStream).bufferedReader(Charsets.UTF_8).use { it.readText() }.escapeHtml()
-        else -> inputStream.bufferedReader(Charsets.UTF_8).use(BufferedReader::readText).escapeHtml()
-    }
+
+fun containsSupported(contentType: String): Boolean =
+    listOf("text", "json", "xml", "yaml").any { contentType.contains(it) }
+
 
 fun String.normalise(): String =
-    replace("""Apache-HttpClient.*""".toRegex(), "http_client")
-        .replace("""#.*""".toRegex(), "")
-        .replace("""\.local.*""".toRegex(), "")
-        .replace(""":.*""".toRegex(), "")
-        .replace("""[$,.:]""".toRegex(), "")
+    replace("""Apache-HttpClient.*""".toRegex(), "http_client").replace("""#.*""".toRegex(), "")
+        .replace("""\.local.*""".toRegex(), "").replace(""":.*""".toRegex(), "").replace("""[$,.:]""".toRegex(), "")
         .replace("-", "_")
 
--- src/test/kotlin/com/lsd/interceptors/http4k/LsdFilterTest.kt
+++ src/test/kotlin/com/lsd/interceptors/http4k/LsdFilterTest.kt
@@ -9,9 +9,14 @@ import com.lsd.core.domain.SequenceEvent
 import org.assertj.core.api.Assertions.assertThat
 import org.http4k.core.*
 import org.http4k.core.Method.GET
+import org.http4k.core.Status.Companion.CONTINUE
+import org.http4k.core.Status.Companion.FOUND
+import org.http4k.core.Status.Companion.INTERNAL_SERVER_ERROR
+import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.MethodSource
 
 class LsdFilterTest {
@@ -25,12 +30,15 @@ class LsdFilterTest {
         }
     }
 
+    private val requestBody = "A request body"
+    private val responseBody = "A response body"
     private val anonymous = PARTICIPANT.called("Anonymous")
     private val unknown = PARTICIPANT.called("Unknown")
     private val bond = PARTICIPANT.called("Bond")
     private val shop = PARTICIPANT.called("Shop")
     private val lsd = SpyContext()
     private val lsdFilter = LsdFilterProvider(lsd).filter
+
 
     @Test
     fun sourceNameAndTargetNamesAreDefaultedIfHeadersAreMissing() {
@@ -46,6 +54,70 @@ class LsdFilterTest {
         lsd.responseMessage().let {
             assertThat(it.to).isEqualTo(anonymous)
             assertThat(it.from).isEqualTo(unknown)
+        }
+    }
+
+    @Test
+    fun compressedContentIsNotShown() {
+        val filterIgnoringCompressedBody = LsdFilterProvider(
+            lsd = lsd,
+            showCompressedBody = false
+        ).filter
+
+        val app = filterIgnoringCompressedBody.then {
+            Response(OK).body(responseBody)
+                .header("Content-Encoding", "compress")
+        }
+
+        app(
+            Request(GET, "/").body(requestBody)
+                .header("Content-Encoding", "gzip")
+        )
+
+        lsd.requestMessage().let {
+            assertThat(it.data as String)
+                .doesNotContain(requestBody)
+                .contains("[encoded body: gzip]")
+        }
+
+        lsd.responseMessage().let {
+            assertThat(it.data as String)
+                .doesNotContain(responseBody)
+                .contains("[encoded body: compress]")
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "application/pdf",
+        "application/octet-stream",
+        "multipart/mixed",
+        "image/gif",
+        "audio/mpeg"
+    )
+    fun notShowBodyForBinaryContent(contentType: String) {
+        val app = lsdFilter.then {
+            Response(OK)
+                .body(responseBody)
+                .header("Content-Type", contentType)
+        }
+
+        app(
+            Request(GET, "/")
+                .body(requestBody)
+                .header("Content-Type", contentType)
+        )
+
+        lsd.requestMessage().let {
+            assertThat(it.data as String)
+                .contains("[$contentType]")
+                .doesNotContain(requestBody)
+        }
+
+        lsd.responseMessage().let {
+            assertThat(it.data as String)
+                .contains("[$contentType]")
+                .doesNotContain(responseBody)
         }
     }
 
@@ -69,13 +141,17 @@ class LsdFilterTest {
     @ParameterizedTest
     @MethodSource("testScenarios")
     fun captureRequestAndResponse(scenario: Scenario) {
-        val app = lsdFilter.then { Response(scenario.status).body("[response body]") }
+        val app = lsdFilter.then {
+            Response(scenario.status)
+                .header("Content-Type", scenario.contentType.value)
+                .body(responseBody)
+        }
 
         app(aGetRequest())
-        
+
         assertThat(lsd.messages).hasSize(2)
-        assertRequestMessage(lsd.messages[0] as Message)
-        assertResponseMessage(lsd.messages[1] as Message, scenario)
+        assertRequestMessage(lsd.requestMessage())
+        assertResponseMessage(lsd.responseMessage(), scenario)
     }
 
     private fun aGetRequest() =
@@ -104,24 +180,26 @@ class LsdFilterTest {
             assertThat(it.label).contains(scenario.status.description)
             assertThat(it.type).isEqualTo(SYNCHRONOUS_RESPONSE)
             assertThat(it.colour).isEqualTo(scenario.arrowColour)
-            assertThat(it.data.toString()).contains("[response body]")
+            assertThat(it.data.toString()).contains(responseBody)
         }
     }
 
     companion object {
         @JvmStatic
         fun testScenarios(): Set<Scenario> {
-            return setOf<Scenario>() + (100..199 step 3).map {
-                Scenario(Status(code = it, description = "status description for $it"), arrowColour = "black")
-            } + (200..299 step 3).map {
-                Scenario(Status(code = it, description = "status description for $it"), arrowColour = "green")
-            } + (300..399 step 3).map {
-                Scenario(Status(code = it, description = "status description for $it"), arrowColour = "purple")
-            } + (400..499 step 3).map {
-                Scenario(Status(code = it, description = "status description for $it"), arrowColour = "orange")
-            } + (500..599 step 3).map {
-                Scenario(Status(code = it, description = "status description for $it"), arrowColour = "red")
-            }
+            return setOf(
+                Scenario(CONTINUE, arrowColour = "black", ContentType.TEXT_PLAIN),
+                Scenario(OK, arrowColour = "green", ContentType.TEXT_PLAIN),
+                Scenario(OK, arrowColour = "green", ContentType.TEXT_XML),
+                Scenario(OK, arrowColour = "green", ContentType.TEXT_HTML),
+                Scenario(OK, arrowColour = "green", ContentType.TEXT_CSV),
+                Scenario(OK, arrowColour = "green", ContentType.APPLICATION_JSON),
+                Scenario(OK, arrowColour = "green", ContentType.APPLICATION_XML),
+                Scenario(OK, arrowColour = "green", ContentType.APPLICATION_YAML),
+                Scenario(FOUND, arrowColour = "purple", ContentType.TEXT_PLAIN),
+                Scenario(NOT_FOUND, arrowColour = "orange", ContentType.TEXT_PLAIN),
+                Scenario(INTERNAL_SERVER_ERROR, arrowColour = "red", ContentType.TEXT_PLAIN)
+            )
         }
     }
 }
@@ -136,4 +214,9 @@ private fun LsdFilterTest.SpyContext.responseMessage(): Message {
     return messages[1] as Message
 }
 
-data class Scenario(val status: Status, val arrowColour: String)
+data class Scenario(
+    val status: Status,
+    val arrowColour: String,
+    val contentType: ContentType
+)
+    
